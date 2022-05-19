07ce7375979e0b01e49b41744c2b10d6e1a4b12a
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-19 14:17:03 +0300

    upadted the views with the mail message capability.

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import LoginForm,RegistrationForm
 from .. import db
-# from ..email import mail_message
+from ..email import mail_message
 
 
 
@@ -31,7 +31,7 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
-        # mail_message("Welcome to one minute pitch","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Blog Play","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
