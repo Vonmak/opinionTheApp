3fa299688001bd4e1e88025b41ebef19e6a4a5e0
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-19 17:02:15 +0300

    last migration.

--- migrations/versions/14bc299d4253_initial_migration.py
+++ migrations/versions/14bc299d4253_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial Migration
 
-Revision ID: 58cd909e5844
+Revision ID: 14bc299d4253
 Revises: 
-Create Date: 2022-05-19 07:02:41.135915
+Create Date: 2022-05-19 16:40:05.091400
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '58cd909e5844'
+revision = '14bc299d4253'
 down_revision = None
 branch_labels = None
 depends_on = None
