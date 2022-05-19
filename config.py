b2ef091380003811260b143e9859a13ac0161446
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-19 14:09:57 +0300

    added the configurations for running the app.

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+import re
+
+
+
+class Config:
+    QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+class ProdConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        # SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://",1)
+    pass
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sarah:sarah@localhost/opinions'
+    DEBUG = True
+    
+config_options ={
+    'development': DevConfig,
+    'production': ProdConfig,
+    # 'test': TestConfig
+}        
