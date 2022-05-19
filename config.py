0c8b246c5010bc7de9e6cb5a125ff9a10362fb0a
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-19 17:06:41 +0300

    change to production mode.

--- config.py
+++ config.py
@@ -18,10 +18,10 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
 class ProdConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    # if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        # SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://",1)
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://",1)
+    # pass
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sarah:sarah@localhost/opinions'
